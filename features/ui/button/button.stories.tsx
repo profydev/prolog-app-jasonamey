6c15aef0108ae109298a762c75759551b211bbe9
jasonamey <34172034+jasonamey@users.noreply.github.com>
2022-12-30 16:35:11 -0500

    refactor default button in sidebar, add button to ui kit (#12)

---
+++ features/ui/button/button.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import {
+  Button,
+  ButtonProps,
+  ButtonSize,
+  ButtonColor,
+  IconPosition,
+} from "./button";
+
+const meta: Meta = {
+  title: "UI/Button",
+  component: Button,
+};
+
+export default meta;
+
+const Template: Story<ButtonProps> = ({
+  size = ButtonSize.small,
+  color = ButtonColor.primary,
+  position = IconPosition.only,
+  children,
+  ...buttonProps
+}: ButtonProps) => (
+  <Button size={size} color={color} position={position} {...buttonProps}>
+    {children}
+  </Button>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: "Button CTA",
+  disabled: false,
+};
